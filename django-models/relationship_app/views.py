9d2549683796530eecb53e0c9c0c3e010a5150f3
Cadeau Mundoringisa <cadeaudedeu@gmail.com>
2024-08-17 18:08:59 +0200

    add views and URL configuration to relationship_app.

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book, Library
+from .models import Book
 from django.views.generic import DetailView   # DetailView is django built in view that displays details about a single object here which is Library 
 # Create your views here.
 
@@ -15,11 +15,11 @@ def home(request):
 
 def list_books(request):  # the function takes the request and fetches all the books in the database
     books = Book.objects.all()  # instruction to fetch all the books in database
-    return render(request, 'list_books.html', {'books': books})  # this tells Django to pass the list_books.html and pass the list of books to it.
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # this tells Django to pass the list_books.html and pass the list of books to it.
 
 # CLASS-BASED VIEW
 
 class LibraryDetailView(DetailView):  # The LibraryDetailView is a class-based view that shows detail about a specific library
     model = Library  # this tells Django that this view is for the library model
-    template_name = 'Library_detail.html'  # template_name is used to render the view
+    template_name = 'relationship_app/library_detail.html'  # template_name is used to render the view
     context_object_name =  'library'  # specifies the name the template will use to access the library object
