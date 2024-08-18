c990669d271b2c886e8e281286c1ff4cc5d1df4a
Cadeau Mundoringisa <cadeaudedeu@gmail.com>
2024-08-18 18:12:50 +0200

    Add the new files.

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
-def admin_check(user):
+def is_admin(user):
     return user.userprofile.role == 'Admin'
 
-@user_passes_test(admin_check)
+@user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    return render(request, 'admin_view.html')
