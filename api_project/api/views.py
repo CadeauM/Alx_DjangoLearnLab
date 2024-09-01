67897775a40838d18da0e5054e304db3012c1656
Cadeau Mundoringisa <cadeaudedeu@gmail.com>
2024-09-01 18:34:00 +0200

    add file that expands API functionality using DRF ViewSets and Routers to implement CRUD operations to the book model.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book  # Imports the Book model
 from .serializers import BookSerializer  # Imports the BookSerializer from the serializer file
 
@@ -8,4 +9,8 @@ from .serializers import BookSerializer  # Imports the BookSerializer from the s
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Query all Book objects
     serializer_class = BookSerializer  # Specifies the serializer to use
+
+class BookViewSet(viewsets.ModelViewSet):  # This is a viewset for viewing and editing book instances
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
 
