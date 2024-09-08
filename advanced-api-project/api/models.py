3b6fdc8f8ef15aa7b38fd1a0f23e40f852de1bbf
Cadeau Mundoringisa <cadeaudedeu@gmail.com>
2024-09-08 23:35:37 +0200

    add file that focuses on creating custom serializers that handle complex data structures and nested relationships.

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)  # Author's name
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)  # Book title
+    publication_year = models.IntegerField()  # Publication year
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')  # One-to-many relationship
+
+    def __str__(self):
+        return self.title
+
