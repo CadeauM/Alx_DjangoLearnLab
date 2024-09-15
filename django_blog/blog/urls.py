350c6392bb9f03bb866e2af88a958ffb6443d272
Cadeau <Cadeaudedeu@gmail.com>
2024-09-15 23:27:48 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostByTagListView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('posts/<int:post_id>/comments/new/', views.add_comment, name='add_comment'),  # This URL is for adding a new comment to a post.
     path('comment/<int:pk>/update/', views.update_comment, name='update_comment'), 
