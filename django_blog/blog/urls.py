fb08adf811b2e27a4ccdaa679d1b28825c61b2c0
Cadeau Mundoringisa <cadeaudedeu@gmail.com>
2024-09-15 20:16:16 +0200

    add the upadated url and html template files.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views  # we are importing the login/logout views
 from . import views   # we are importing the views we have create for registration and profile.
-
+from .views import PostListView
+from .views import PostDetailView
+from .views import PostCreateView
+from .views import PostUpdateView
+from .views import PostDeleteView
 # Defining the paths for login, logout, registration, and profile
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'), 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'), 
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
 ]
