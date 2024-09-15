f7b584efa3741db15a1cbe6d5004780b6db8ed75
Cadeau Mundoringisa <cadeaudedeu@gmail.com>
2024-09-15 17:55:49 +0200

    fix error shown by code checker.

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_blog_db',
+        'USER': 'my_blog_db',
+        'PASSWORD': 'Cadeau@0707',
+        'HOST': 'localhost', 
+        'PORT': '5050',
     }
 }
 
