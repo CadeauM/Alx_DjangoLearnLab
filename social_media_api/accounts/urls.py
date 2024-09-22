52876a8ef9098c96a6509466be999e1e2cb3b9ff
Cadeau <Cadeaudedeu@gmail.com>
2024-09-22 18:46:18 +0200

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,5 +5,5 @@ from .views import LoginView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'), 
-
+    path('login/', CustomAuthToken.as_view(), name='login'),
 ]
