d6c3f6a063cd96811cfce5576fa5cbc74c93bfcd
Cadeau <Cadeaudedeu@gmail.com>
2024-09-22 18:06:19 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,21 @@
 # Serializers are used to convert complex data types (like models) into JSON (a format that's easy to work with in APIs).
 from rest_framework import serializers
 from .models import CustomUser
+from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'bio', 'profile_picture', 'followers']
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    token = serializers.CharField(read_only=True)
+    class Meta: 
+        model = User
+        fields = ('username', 'email', 'password', 'token')
+        def create(self, validated_data): # Create a new user using the validated data
+            user = User.objects.create_user( username=validated_data['username'], email=validated_data.get('email'), password=validated_data['password'] ) # Generate a token for the user 
+            token = Token.objects.create(user=user) 
+            user.token = token.key
+            return user
+    
