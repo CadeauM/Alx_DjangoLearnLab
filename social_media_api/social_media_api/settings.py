008c7180617b6371a23167f564598ae735c872a5
Cadeau <Cadeaudedeu@gmail.com>
2024-09-29 23:25:24 +0200

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i=(=g*b&t11j^a#mws9%x)#yuc3zg(gtje05ehlqd)$r63gr*r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
